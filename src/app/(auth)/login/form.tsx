a2dcecb41519dabda893ce420b1dfa40dfba0556
Wil Macedo <wil.macedo.sa@gmail.com>
2023-12-04 09:24:05 -0300

    feat: :zap: update register to use hook form

--- src/app/(auth)/login/form.tsx
+++ src/app/(auth)/login/form.tsx
@@ -5,45 +5,43 @@ import { GoogleLogo } from "@/components/google-logo";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { useToast } from "@/components/ui/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
+import { ChangeEvent, useState } from "react";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 type Loading = "google" | "email";
 
-const schema = z.object({
-  email: z.string().email(),
+const loginUserFormSchema = z.object({
+  email: z.string().nonempty().email(),
+  password: z.string().nonempty().min(3),
 });
+
+type LoginUserFormData = z.infer<typeof loginUserFormSchema>;
 
 export function Form() {
   const [loading, setLoading] = useState<Loading | null>(null);
+  const [step, setStep] = useState(0);
+
   const router = useRouter();
   const { toast } = useToast();
 
-  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+  const { register, handleSubmit } = useForm<LoginUserFormData>({
+    resolver: zodResolver(loginUserFormSchema),
+  });
 
-    const input = event.currentTarget.querySelector(
-      "#email"
-    ) as HTMLInputElement;
-
-    const parsedData = schema.safeParse({ email: input.value });
-    if (!parsedData.success) {
-      toast({
-        variant: "destructive",
-        title: "O e-mail informado não é válido.",
-      });
-      return;
-    }
+  async function authorizeUser(data: LoginUserFormData) {
+    const { email, password } = data;
 
     setLoading("email");
 
     const response = await signIn("credentials", {
       callbackUrl: "/discover",
-      email: parsedData.data.email,
-      password: "123456",
+      email,
+      password,
       redirect: false,
     });
 
@@ -65,9 +63,13 @@ export function Form() {
     signIn("google", { callbackUrl: "/discover" });
   }
 
+  function handleEmailInput(event: ChangeEvent<HTMLInputElement>) {
+    setStep(event.target.value.length > 0 ? 1 : 0);
+  }
+
   return (
     <form
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(authorizeUser)}
       className="h-full flex items-center justify-center"
     >
       <div className="text-center">
@@ -75,40 +77,68 @@ export function Form() {
         <p className="mt-2 text-sm">Entre com sua conta via e-mail ou Google</p>
 
         <div className="mt-4">
-          <Input id="email" type="email" placeholder="nome@exemplo.com" />
-          <Button
-            type="submit"
-            className="mt-1 w-full py-2 flex items-center justify-center gap-2 bg-[#D9EAB8] text-sm disabled:hover:bg-[#D9EAB8]"
-            disabled={loading !== null}
+          <div
+            data-credentials={step > 0}
+            className="data-[credentials=true]:space-y-4"
           >
-            {loading === "email" && (
-              <Loader2 className="h-4 w-4 animate-spin" />
-            )}
-            Entrar com Email
-          </Button>
+            <Input
+              id="email"
+              type="email"
+              placeholder="nome@exemplo.com"
+              {...register("email", {
+                onChange: handleEmailInput,
+              })}
+            />
 
-          <div className="relative flex items-center justify-center my-4">
-            <Separator className="absolute left-0" orientation="horizontal" />
+            <Input
+              data-credentials={step > 0}
+              minLength={3}
+              id="password"
+              type="password"
+              placeholder="Senha"
+              className="h-0 py-0 opacity-0 invisible data-[credentials=true]:py-2 data-[credentials=true]:h-10 data-[credentials=true]:opacity-100 data-[credentials=true]:visible transition-all duration-150"
+              {...register("password")}
+            />
 
-            <span className="px-4 z-10 text-muted-foreground bg-background text-sm uppercase">
-              ou continue com
-            </span>
+            <Button
+              type="submit"
+              className="mt-1 w-full py-2 flex items-center justify-center gap-2 bg-[#D9EAB8] text-sm disabled:hover:bg-[#D9EAB8]"
+              disabled={loading !== null}
+            >
+              {loading === "email" && (
+                <Loader2 className="h-4 w-4 animate-spin" />
+              )}
+              Entrar com Email
+            </Button>
           </div>
 
-          <Button
-            onClick={handleSocial}
-            className="w-full py-2 flex items-center justify-center gap-2 text-sm"
-            variant="outline"
-            disabled={loading !== null}
+          <div
+            data-credentials={step > 0}
+            className="opacity-100 visible data-[credentials=true]:opacity-0 data-[credentials=true]:invisible transition-all duration-150"
           >
-            {(loading === null || loading !== "google") && (
-              <GoogleLogo className="mr-2 h-4 w-4" />
-            )}
-            {loading === "google" && (
-              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Entrar com Google
-          </Button>
+            <div className="relative flex items-center justify-center my-4">
+              <Separator className="absolute left-0" orientation="horizontal" />
+
+              <span className="px-4 z-10 text-muted-foreground bg-background text-sm uppercase">
+                ou continue com
+              </span>
+            </div>
+
+            <Button
+              onClick={handleSocial}
+              className="w-full py-2 flex items-center justify-center gap-2 text-sm"
+              variant="outline"
+              disabled={loading !== null}
+            >
+              {(loading === null || loading !== "google") && (
+                <GoogleLogo className="mr-2 h-4 w-4" />
+              )}
+              {loading === "google" && (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Entrar com Google
+            </Button>
+          </div>
         </div>
       </div>
     </form>
